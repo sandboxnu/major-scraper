b7ba30c6163e159c0daa63f1bf6d68508215b509
Luke Taylor <luke@bigring.org>
2024-01-25 00:54:22 -0500

    Add basic major-read Tauri command

--- major-cleaner/src-tauri/src/main.rs
+++ major-cleaner/src-tauri/src/main.rs
@@ -1,15 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::fs;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_major_file(year: &str, college: &str, name: &str, form: &str) -> String {
+    // TODO: ensure each path segment doesn't contain special path chars (ie. /, .., etc.)
+    let path = format!("../../degrees/Major/{year}/{college}/{name}/{form}");
+    match fs::read_to_string(&path) {
+        Ok(str) => str,
+        Err(err) => err.to_string()
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_major_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
