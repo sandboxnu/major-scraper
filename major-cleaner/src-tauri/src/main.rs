2a39070b2cfb790e2407a850a244b7190301dd90
Luke Taylor <luke@bigring.org>
2024-01-25 01:21:33 -0500

    Add some TODOs in main.rs

--- major-cleaner/src-tauri/src/main.rs
+++ major-cleaner/src-tauri/src/main.rs
@@ -3,15 +3,20 @@
 
 use std::fs;
 
+// TODO: investigate strong typing for Tauri commands (maybe something like https://github.com/oscartbeaumont/tauri-specta ?).
+
 #[tauri::command]
 fn read_major_file(year: &str, college: &str, name: &str, form: &str) -> String {
     // TODO: ensure each path segment doesn't contain special path chars (ie. /, .., etc.)
     let path = format!("../../degrees/Major/{year}/{college}/{name}/{form}");
     match fs::read_to_string(&path) {
         Ok(str) => str,
+        // TODO: setup a consistent error/normal result format to allow the frontend to read and display potential errors.
         Err(err) => err.to_string()
     }
 }
+
+// TODO: make a write_major_file function
 
 fn main() {
     tauri::Builder::default()
